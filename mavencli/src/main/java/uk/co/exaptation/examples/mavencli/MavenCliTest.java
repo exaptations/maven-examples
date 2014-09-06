e10884145f2da798a39d268b5b43d6e08e8c615d
exaptations <exaptations@gmail.com>
2014-09-06 16:36:48 +0100

    mavencli commit

---
+++ mavencli/src/main/java/uk/co/exaptation/examples/mavencli/MavenCliTest.java
@@ -0,0 +1,19 @@
+package uk.co.exaptation.examples.mavencli;
+
+import org.apache.maven.cli.MavenCli;
+
+public class MavenCliTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		cliTest1();
+	}
+
+	private static void cliTest1() {
+		MavenCli cli = new MavenCli();
+		int result = cli.doMain(new String[] { "compile","dependency:tree","-Ddetail=true" }, "./", System.out, System.out);
+	}
+
+}
